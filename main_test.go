a335222432b83f063746c0f88522020d169a6a05
joetifa2003 <62658783+joetifa2003@users.noreply.github.com>
2022-12-01 00:08:41 +0200

    fix: remove ManualBenchmark

--- main_test.go
+++ main_test.go
@@ -66,40 +66,6 @@ func BenchmarkArenaManual(b *testing.B) {
 	}
 }
 
-// Example of cgo overhead makes it actually slower than go
-// don't alloc and free in a hot loop and try to allocate in chunks
-// to avoid cgo overhead (look at BenchmarkArenaManual)
-func BenchmarkManual(b *testing.B) {
-	for i := b.N; i <= b.N; i++ {
-		nodes := make([]*Node, NNodes)
-
-		for j := 0; j < NNodes; j++ {
-			var prev *Node
-			var next *Node
-			if j != 0 {
-				prev = nodes[j-1]
-			}
-			if j != NNodes-1 {
-				next = nodes[j+1]
-			}
-
-			node := Alloc[Node]()
-			*node = Node{
-				Value: j,
-				Prev:  prev,
-				Next:  next,
-			}
-			nodes[j] = node
-		}
-
-		for _, n := range nodes {
-			Free(n)
-		}
-
-		runtime.GC()
-	}
-}
-
 func TestAllocMany(t *testing.T) {
 	assert := assert.New(t)
 
