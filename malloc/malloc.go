3f51ea18cee646907f52253dd5008c8b27a76fe6
joetifa2003 <62658783+joetifa2003@users.noreply.github.com>
2022-12-17 04:18:26 +0200

    feat: malloc pacakge

---
+++ malloc/malloc.go
@@ -0,0 +1,20 @@
+package malloc
+
+// #include <stdlib.h>
+import "C"
+import "unsafe"
+
+// CMalloc raw binding to c calloc(1, size)
+func Malloc(size int) unsafe.Pointer {
+	return C.calloc(1, C.size_t(size))
+}
+
+// CMalloc raw binding to c free
+func Free(ptr unsafe.Pointer) {
+	C.free(ptr)
+}
+
+// CMalloc raw binding to c realloc
+func Realloc(ptr unsafe.Pointer, size int) unsafe.Pointer {
+	return C.realloc(ptr, C.size_t(size))
+}
