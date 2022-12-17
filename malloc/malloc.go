1621d433b63601a07e7adedba207e0b2298685cf
joetifa2003 <62658783+joetifa2003@users.noreply.github.com>
2022-12-17 04:26:06 +0200

    feat: include mimalloc src only

--- malloc/malloc.go
+++ malloc/malloc.go
@@ -5,14 +5,17 @@ package malloc
 import "C"
 import "unsafe"
 
+// CMalloc raw binding to mimalloc calloc(1, size)
 func CMalloc(size int) unsafe.Pointer {
 	return C.mi_calloc(1, C.size_t(size))
 }
 
+// CMalloc raw binding to mimalloc free
 func CFree(ptr unsafe.Pointer) {
 	C.mi_free(ptr)
 }
 
+// CMalloc raw binding to mimalloc realloc
 func CRealloc(ptr unsafe.Pointer, size int) unsafe.Pointer {
 	return C.mi_realloc(ptr, C.size_t(size))
 }
