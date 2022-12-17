8c25a0cf6ac654e4a6e55e3ad131dda48c75901c
joetifa2003 <62658783+joetifa2003@users.noreply.github.com>
2022-12-17 04:28:54 +0200

    feat: mimalloc package

--- mm.go
+++ mm.go
@@ -13,21 +13,21 @@ func getSize[T any]() int {
 
 // Alloc allocates T and returns a pointer to it.
 func Alloc[T any]() *T {
-	ptr := malloc.CMalloc(getSize[T]())
+	ptr := malloc.Malloc(getSize[T]())
 	return (*T)(unsafe.Pointer(ptr))
 }
 
 // FreeMany frees memory allocated by Alloc takes a ptr
 // CAUTION: be careful not to double free, and prefer using defer to deallocate
 func Free[T any](ptr *T) {
-	malloc.CFree(unsafe.Pointer(ptr))
+	malloc.Free(unsafe.Pointer(ptr))
 }
 
 // AllocMany allocates n of T and returns a slice representing the heap.
 // CAUTION: don't append to the slice, the purpose of it is to replace pointer
 // arithmetic with slice indexing
 func AllocMany[T any](n int) []T {
-	ptr := malloc.CMalloc(getSize[T]() * n)
+	ptr := malloc.Malloc(getSize[T]() * n)
 	return unsafe.Slice(
 		(*T)(ptr),
 		n,
@@ -37,12 +37,12 @@ func AllocMany[T any](n int) []T {
 // FreeMany frees memory allocated by AllocMany takes in the slice (aka the heap)
 // CAUTION: be careful not to double free, and prefer using defer to deallocate
 func FreeMany[T any](slice []T) {
-	malloc.CFree(unsafe.Pointer(&slice[0]))
+	malloc.Free(unsafe.Pointer(&slice[0]))
 }
 
 // Reallocate reallocates memory allocated with AllocMany and doesn't change underling data
 func Reallocate[T any](slice []T, newN int) []T {
-	ptr := malloc.CRealloc(unsafe.Pointer(&slice[0]), getSize[T]()*newN)
+	ptr := malloc.Realloc(unsafe.Pointer(&slice[0]), getSize[T]()*newN)
 	return unsafe.Slice(
 		(*T)(ptr),
 		newN,
