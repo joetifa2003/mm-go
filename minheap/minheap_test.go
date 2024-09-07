30e991a77e001d28a38ae6cbbe4b70e289a9b144
Youssef Ahmed <joetifa2003@gmail.com>
2024-09-08 00:14:50 +0200

    feat: add minheap datastructure

---
+++ minheap/minheap_test.go
@@ -0,0 +1,17 @@
+package minheap
+
+import (
+	"testing"
+
+	"github.com/joetifa2003/mm-go/allocator"
+)
+
+func TestMinHeap(t *testing.T) {
+	alloc := allocator.NewCallocator()
+	heap := New[int](alloc, func(a, b int) bool { return a < b })
+
+	heap.Push(3)
+	heap.Push(4)
+	heap.Push(1)
+	heap.Push(0)
+}
