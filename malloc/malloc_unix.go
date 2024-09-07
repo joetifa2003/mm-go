f01ba03927ed9a0dcd720677f0a93e6c71aa0cab
Youssef Ahmed <joetifa2003@gmail.com>
2024-09-07 12:50:26 +0200

    fix: use CGO instead of purgo

--- malloc/malloc_unix.go
+++
@@ -1,9 +0,0 @@
-//go:build darwin || linux
-
-package malloc
-
-import "github.com/ebitengine/purego"
-
-func openLibrary(name string) (uintptr, error) {
-	return purego.Dlopen(name, purego.RTLD_NOW|purego.RTLD_GLOBAL)
-}
