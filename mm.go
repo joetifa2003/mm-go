dfbd53055f79e6afe7d6b611447d4233c47e91bb
Youssef Ahmed <joetifa2003@gmail.com>
2024-09-08 16:05:22 +0200

    feat: experimenting with new benchmarks

--- mm.go
+++ mm.go
@@ -1,1 +1,8 @@
 package mm
+
+import "unsafe"
+
+func SizeOf[T any]() int {
+	var zeroV T
+	return int(unsafe.Sizeof(zeroV))
+}
--- mm_test.go
+++ mm_test.go
@@ -5,83 +5,133 @@ import (
 	"runtime"
 	"testing"
 
+	"github.com/joetifa2003/mm-go"
 	"github.com/joetifa2003/mm-go/allocator"
 	"github.com/joetifa2003/mm-go/batchallocator"
-	"github.com/joetifa2003/mm-go/typedarena"
 )
 
-type TreeNode struct {
-	value       int
-	left, right *TreeNode
+type Node[T any] struct {
+	value T
+	next  *Node[T]
+	prev  *Node[T]
 }
 
-func createTreeManaged(depth int) *TreeNode {
-	if depth != 0 {
-		return &TreeNode{
-			value: depth,
-			left:  createTreeManaged(depth - 1),
-			right: createTreeManaged(depth - 1),
-		}
+type LinkedList[T any] struct {
+	head *Node[T]
+	tail *Node[T]
+}
+
+func linkedListPushManaged[T any](list *LinkedList[T], value T) {
+	node := &Node[T]{value: value}
+	if list.head == nil {
+		list.head = node
+		list.tail = node
+	} else {
+		list.tail.next = node
+		node.prev = list.tail
+		list.tail = node
 	}
-
-	return nil
 }
 
-func createTreeManual(depth int, arena *typedarena.TypedArena[TreeNode]) *TreeNode {
-	if depth != 0 {
-		node := arena.Alloc()
-		node.left = createTreeManual(depth-1, arena)
-		node.right = createTreeManual(depth-1, arena)
-		return node
+func linkedListPushAlloc[T any](alloc allocator.Allocator, list *LinkedList[T], value T) {
+	node := allocator.Alloc[Node[T]](alloc)
+	node.value = value
+
+	if list.head == nil {
+		list.head = node
+		list.tail = node
+	} else {
+		list.tail.next = node
+		node.prev = list.tail
+		list.tail = node
 	}
-
-	return nil
 }
 
-func sumBinaryTree(tree *TreeNode) int {
-	if tree.left == nil && tree.right == nil {
-		return tree.value
+func linkedListFree[T any](alloc allocator.Allocator, list *LinkedList[T]) {
+	currentNode := list.head
+	for currentNode != nil {
+		nextNode := currentNode.next
+		allocator.Free(alloc, currentNode)
+		currentNode = nextNode
 	}
-
-	return sumBinaryTree(tree.left) + sumBinaryTree(tree.right)
+	allocator.Free(alloc, list)
 }
 
-const TREE_DEPTH = 13
+const LINKED_LIST_SIZE = 10000
 
-func BenchmarkBinaryTreeManaged(b *testing.B) {
-	for n := 0; n < b.N; n++ {
-		tree := createTreeManaged(TREE_DEPTH)
+func BenchmarkLinkedListManaged(b *testing.B) {
+	for range b.N {
+		benchLinkedListManaged(b, LINKED_LIST_SIZE)
 		runtime.GC()
-		sumBinaryTree(tree)
 	}
 }
 
-func BenchmarkBinaryTreeArena(b *testing.B) {
-	for _, chunkSize := range []int{50, 100, 150, 250, 500} {
-		b.Run(fmt.Sprintf("callocator chunk size %d", chunkSize), func(b *testing.B) {
-			for n := 0; n < b.N; n++ {
-				alloc := allocator.NewCallocator()
-				arena := typedarena.New[TreeNode](alloc, chunkSize)
-				tree := createTreeManual(TREE_DEPTH, arena)
-				runtime.GC()
-				sumBinaryTree(tree)
-				arena.Free()
-				alloc.Destroy()
+func BenchmarkLinkedListCAlloc(b *testing.B) {
+	for range b.N {
+		benchLinkedListCAlloc(b, LINKED_LIST_SIZE)
+	}
+}
+
+func BenchmarkLinkedListBatchAllocator(b *testing.B) {
+	for _, bucketSize := range []int{100, 200, 500, LINKED_LIST_SIZE} {
+		b.Run(fmt.Sprintf("bucket size %d", bucketSize), func(b *testing.B) {
+			for range b.N {
+				benchLinkedListBatchAllocator(b, LINKED_LIST_SIZE, bucketSize)
 			}
 		})
 	}
+}
 
-	for _, chunkSize := range []int{50, 100, 150, 250, 500} {
-		b.Run(fmt.Sprintf("batchallocator chunk size %d", chunkSize), func(b *testing.B) {
-			for n := 0; n < b.N; n++ {
-				alloc := batchallocator.New(allocator.NewCallocator())
-				arena := typedarena.New[TreeNode](alloc, chunkSize)
-				tree := createTreeManual(TREE_DEPTH, arena)
-				runtime.GC()
-				sumBinaryTree(tree)
-				arena.Free()
-				alloc.Destroy()
-			}
-		})
+func benchLinkedListManaged(b *testing.B, size int) {
+	list := &LinkedList[int]{}
+	for i := range size {
+		linkedListPushManaged(list, i)
+	}
+	assertLinkedList(b, list)
+}
+
+func benchLinkedListCAlloc(b *testing.B, size int) {
+	alloc := allocator.NewCallocator()
+	defer alloc.Destroy()
+
+	list := allocator.Alloc[LinkedList[int]](alloc)
+	defer linkedListFree(alloc, list)
+
+	for i := range size {
+		linkedListPushAlloc(alloc, list, i)
+	}
+
+	assertLinkedList(b, list)
+}
+
+func benchLinkedListBatchAllocator(b *testing.B, size int, bucketSize int) {
+	alloc := batchallocator.New(allocator.NewCallocator(),
+		batchallocator.WithBucketSize(mm.SizeOf[Node[int]]()*bucketSize),
+	)
+	defer alloc.Destroy()
+
+	list := allocator.Alloc[LinkedList[int]](alloc)
+	for i := range size {
+		linkedListPushAlloc(alloc, list, i)
+	}
+	assertLinkedList(b, list)
+}
+
+func assertLinkedList(t *testing.B, list *LinkedList[int]) {
+	if list.head == nil {
+		t.Fatal("list head is nil")
+	}
+	if list.tail == nil {
+		t.Fatal("list tail is nil")
+	}
+
+	currentNode := list.head
+	i := 0
+	for currentNode != nil {
+		if currentNode.value != i {
+			t.Fatalf("list value at index %d is %d, expected %d", i, currentNode.value, i)
+		}
+		i++
+		currentNode = currentNode.next
 	}
 }
